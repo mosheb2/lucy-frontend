2898bc6eaeca7a37138a46d9d0373ce10f2d943c
mosheb2 <moshe.bareket@ex.co>
2025-07-11 17:40:28 +0300

    Update Lucy Frontend code with fixed syntax and module type

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,59 +1,31 @@
 import Layout from "./Layout.jsx";
-
 import Dashboard from "./Dashboard";
-
 import Settings from "./Settings";
-
 import Promotion from "./Promotion";
-
 import Collaborate from "./Collaborate";
-
 import Artist from "./Artist";
-
 import IntegratedAnalytics from "./IntegratedAnalytics";
-
 import SocialFeed from "./SocialFeed";
-
 import Analytics from "./Analytics";
-
 import AdminDashboard from "./AdminDashboard";
-
 import AdminUserManagement from "./AdminUserManagement";
-
 import AdminContentManagement from "./AdminContentManagement";
-
 import AdminPlatformAnalytics from "./AdminPlatformAnalytics";
-
 import AdminPlatformSettings from "./AdminPlatformSettings";
-
 import Explore from "./Explore";
-
 import Menu from "./Menu";
-
 import AdminSubmissions from "./AdminSubmissions";
-
 import YouTubeTool from "./YouTubeTool";
-
 import Studio from "./Studio";
-
 import LandingPage from "./LandingPage";
-
 import AdminSupportRequests from "./AdminSupportRequests";
-
 import Messenger from "./Messenger";
-
 import AdminMentorManagement from "./AdminMentorManagement";
-
 import MusicLibrary from "./MusicLibrary";
-
 import Distribution from "./Distribution";
-
 import SongRegistration from "./SongRegistration";
-
 import Support from "./Support";
-
 import AuthCallback from "./AuthCallback";
-
 import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
@@ -61,61 +33,33 @@ import ConnectWallet from './ConnectWallet';
 import { useAuth } from '@/contexts/AuthContext';
 
 const PAGES = {
-    
     Dashboard: Dashboard,
-    
     Settings: Settings,
-    
     Promotion: Promotion,
-    
     Collaborate: Collaborate,
-    
     Artist: Artist,
-    
     IntegratedAnalytics: IntegratedAnalytics,
-    
     SocialFeed: SocialFeed,
-    
     Analytics: Analytics,
-    
     AdminDashboard: AdminDashboard,
-    
     AdminUserManagement: AdminUserManagement,
-    
     AdminContentManagement: AdminContentManagement,
-    
     AdminPlatformAnalytics: AdminPlatformAnalytics,
-    
     AdminPlatformSettings: AdminPlatformSettings,
-    
     Explore: Explore,
-    
     Menu: Menu,
-    
     AdminSubmissions: AdminSubmissions,
-    
     Layout: Layout,
-    
     YouTubeTool: YouTubeTool,
-    
     Studio: Studio,
-    
     LandingPage: LandingPage,
-    
     AdminSupportRequests: AdminSupportRequests,
-    
     Messenger: Messenger,
-    
     AdminMentorManagement: AdminMentorManagement,
-    
     MusicLibrary: MusicLibrary,
-    
     Distribution: Distribution,
-    
     SongRegistration: SongRegistration,
-    
     Support: Support,
-    
 }
 
 function _getCurrentPage(url) {
@@ -139,67 +83,37 @@ function PagesContent() {
     return (
         <Layout currentPageName={currentPage}>
             <Routes>            
-                
-                    <Route path="/" element={<Dashboard />} />
-                
-                
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/Dashboard" element={<Dashboard />} />
-                
                 <Route path="/Settings" element={<Settings />} />
-                
                 <Route path="/Promotion" element={<Promotion />} />
-                
                 <Route path="/Collaborate" element={<Collaborate />} />
-                
                 <Route path="/Artist" element={<Artist />} />
-                
                 <Route path="/IntegratedAnalytics" element={<IntegratedAnalytics />} />
-                
                 <Route path="/SocialFeed" element={<SocialFeed />} />
-                
                 <Route path="/Analytics" element={<Analytics />} />
-                
                 <Route path="/AdminDashboard" element={<AdminDashboard />} />
-                
                 <Route path="/AdminUserManagement" element={<AdminUserManagement />} />
-                
                 <Route path="/AdminContentManagement" element={<AdminContentManagement />} />
-                
                 <Route path="/AdminPlatformAnalytics" element={<AdminPlatformAnalytics />} />
-                
                 <Route path="/AdminPlatformSettings" element={<AdminPlatformSettings />} />
-                
                 <Route path="/Explore" element={<Explore />} />
-                
                 <Route path="/Menu" element={<Menu />} />
-                
                 <Route path="/AdminSubmissions" element={<AdminSubmissions />} />
-                
                 <Route path="/Layout" element={<Layout />} />
-                
                 <Route path="/YouTubeTool" element={<YouTubeTool />} />
-                
                 <Route path="/Studio" element={<Studio />} />
-                
                 <Route path="/LandingPage" element={<LandingPage />} />
-                
                 <Route path="/AdminSupportRequests" element={<AdminSupportRequests />} />
-                
                 <Route path="/Messenger" element={<Messenger />} />
-                
                 <Route path="/AdminMentorManagement" element={<AdminMentorManagement />} />
-                
                 <Route path="/MusicLibrary" element={<MusicLibrary />} />
-                
                 <Route path="/Distribution" element={<Distribution />} />
-                
                 <Route path="/SongRegistration" element={<SongRegistration />} />
-                
                 <Route path="/Support" element={<Support />} />
                 
                 {/* Catch-all route for 404s */}
                 <Route path="*" element={<Navigate to="/Dashboard" replace />} />
-                
             </Routes>
         </Layout>
     );
@@ -240,7 +154,5 @@ export default function Pages() {
                 } />
             </Routes>
         </Router>
-    );
-}
     );
 }
